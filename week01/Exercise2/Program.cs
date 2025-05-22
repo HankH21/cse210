538c7d7bad802120717ba76c5d3342aec0900402
HankH21 <hankhung9421@gmail.com>
2025-05-23 03:51:22 +0800

    exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        float grade_value = float.Parse(grade);
+        string grade_letter = "";
+
+        if (grade_value >= 90)
+        {
+            grade_letter = "A";
+        }
+
+        else if (grade_value >= 80)
+        {
+            grade_letter = "B";
+        }
+
+        else if (grade_value >= 70)
+        {
+            grade_letter = "C";
+        }
+
+        else if (grade_value >= 60)
+        {
+            grade_letter = "D";
+        }
+
+        else
+        {
+            grade_letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade_letter}");
+
+
+        if (grade_value > 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass this class!");
+        }
+
     }
 }
