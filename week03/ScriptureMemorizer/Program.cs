dfce21286ec105ad66787ddd9273a604b28de570
HankH21 <hankhung9421@gmail.com>
2025-06-08 04:28:57 +0800

    1

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+             Reference reference = new Reference("D&C", 105, 3, 5);
+        Scripture scripture = new Scripture(reference, "Hello guys my name is Pei-Han Hung test test test test test test test test test");
+
+
+        while (!scripture.IsCompleteHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Press enter to hide random words or type 'quit' to exit.");
+            string userInput = Console.ReadLine();
+
+            if (userInput.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords();
+
+        }
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+        Console.WriteLine();
+        Console.WriteLine("All words are hidden. Goodbye!");
+    }
     }
 }
