8c246099c221bfbb2b8b246273a31c9c67db4a68
HankH21 <hankhung9421@gmail.com>
2025-06-14 02:20:39 +0800

    W5 learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("John Doe", "Geography");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment massignment1 = new MathAssignment("Giorno Giovanna", "Algebra I", "9.3", "5-17");
+        Console.WriteLine(massignment1.GetSummary());
+        Console.WriteLine(massignment1.GetHomeworkList());
+
+        WritingAssignment wassignment1 = new WritingAssignment("John Wick", "Reading Report", "The Worst Book");
+        Console.WriteLine(wassignment1.GetSummary());
+        Console.WriteLine(wassignment1.GetWritingInformation());
     }
 }
